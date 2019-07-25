5bff8539b602e982a59e614556310cb6e3939271
Bryce DeBilzan <bdebilzan@mac.com>
2019-07-24 18:08:13 -0700

    Update Not_Boring_Movies_620.sql

--- Not_Boring_Movies_620.sql
+++ Not_Boring_Movies_620.sql
@@ -1,4 +1,9 @@
-# Write your MySQL query statement below
+/* X city opened a new cinema, many people would like to go to this cinema. 
+The cinema also gives out a poster indicating the movies’ ratings and descriptions.
+Please write a SQL query to output movies with an odd numbered ID and a description that is not 'boring'. 
+Order the result by rating.
+
+ Write your MySQL query statement below */
 
 SELECT * FROM cinema
 WHERE (id % 2 = 1)
