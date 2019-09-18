fc7962a35c21287d94f583bc555bd3f72012efd8
Bryce DeBilzan <bdebilzan@mac.com>
2019-09-17 22:25:06 -0700

    Update Height_Checker_1051.java

--- Height_Checker_1051.java
+++ Height_Checker_1051.java
@@ -1,5 +1,4 @@
 /*
-
 Students are asked to stand in non-decreasing order of heights for an annual photo.
 Return the minimum number of students not standing in the right positions.  
 (This is the number of students that must move in order for all students to be standing in non-decreasing order of height.)
